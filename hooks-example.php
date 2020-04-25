90773da36cb3bcf0082e5356d937f8379a4a37d5
Wright <wrightkhlebisol@gmail.com>
2020-04-25 03:06:29 +0100

    Creating custom hooks

--- hooks-example.php
+++ hooks-example.php
@@ -18,9 +18,27 @@ function wu_footer_shoutout()
     echo 'Hooked footer shoutourt was here <br>';
 }
 
-add_action('wp_footer', 'wu_footer_shoutout_v2');
+add_action(
+    'wp_footer',
+    'wu_footer_shoutout_v2',
+    5
+);
 
 function wu_footer_shoutout_v2()
 {
-    echo 'Hooked footer shoutourt was here. Version 2';
+    echo 'Hooked footer shoutourt was here. Version 2 <br>';
+}
+
+add_action('wp_footer', 'wu_footer');
+
+function wu_footer()
+{
+    do_action('wu_custom_footer');
+}
+
+add_action('wu_custom_footer', 'wu_kill_wp');
+
+function wu_kill_wp()
+{
+    echo 'Custom hook';
 }
